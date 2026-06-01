4555d324c1b2b3854ca5ea2a1be8170dc5fed80b
srbh-x <saurabhsunil7@gmail.com>
2026-06-01 23:28:39 +0530

    Added Habivore x 8Verse logo

--- src/components/home-one/footer/FooterBottom.jsx
+++ src/components/home-one/footer/FooterBottom.jsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
-import LogoWhiteImg from "../../../public/images/logo/logo-white.svg";
+import LogoWhiteImg from "../../../public/images/logo/8VerseHABIVORE-light.png";
 function FooterBottom() {
 	return (
 		<>
 			<div className="col-lg-6">
 				<div className="aximo-footer-logo">
-					<a href="#">
+					<a href="https://www.habivore.com" target="_blank" rel="noopener noreferrer">
 						<Image src={LogoWhiteImg} alt="Logo" />
 					</a>
 				</div>
 			</div>
 			<div className="col-lg-6">
 				<div className="aximo-copywright one">
-					<p> &copy; Copyright 2026, All Rights Reserved</p>
+					{/* <p> &copy; Copyright 2026, All Rights Reserved</p> */}
+					<p> Copyright &copy; 2026 The 8 Verse. All Rights Reserved.</p>
 				</div>
 			</div>
 		</>
